92cf043066e3309432c073855ad3a12e33df2c1a
MD Rashid Hussain <mdrashid.hussain.786.01@gmail.com>
2022-02-11 12:14:34 +0530

    remove unwanted

--- client/src/components/MiddleStuffDark/MiddleStuffDark.tsx
+++ client/src/components/MiddleStuffDark/MiddleStuffDark.tsx
@@ -10,7 +10,7 @@ const MiddleStuffDark = () => {
           alt=""
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fetchfind.com%2Fblog%2Fwp-content%2Fuploads%2F2017%2F08%2Fcat-2734999_1920-5-common-cat-sounds.jpg&f=1&nofb=1"
         />
-        <input type="text" placeholder="Create shitty post ..." />
+        <input type="text" placeholder="Create a post ..." />
         <p className="buttonboi">Create</p>
       </div>
       <div className="feeds">
